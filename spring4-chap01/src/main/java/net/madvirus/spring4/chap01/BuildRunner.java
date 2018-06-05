e1824caf1cccddc10a9457c5a166a369f3fc8c7c
injang <injang@github.com>
2018-06-05 17:50:47 +0900

    3.5예제

--- spring4-chap01/src/main/java/net/madvirus/spring4/chap01/BuildRunner.java
+++ spring4-chap01/src/main/java/net/madvirus/spring4/chap01/BuildRunner.java
@@ -2,7 +2,7 @@ package net.madvirus.spring4.chap01;
 
 import java.util.List;
 
-public class BuildRunner {
+public interface BuildRunner {
 	
 	public void build(List<String> srcDirs, String binDir);
 	
